f6b8460f07249387b41f9ad313186275dc20a084
sunakanemai <2021php41-student014@la-study.com>
2021-05-20 11:55:55 +0900

    登録画面完成

--- mamaiko/resources/views/auth/register.blade.php
+++ mamaiko/resources/views/auth/register.blade.php
@@ -15,35 +15,35 @@
         <main>
             <div class=container>
                 <h1>新規会員登録</h1>
-                <form action="{{route('conform')}}" method="post">
+                <form action="{{route('register')}}" method="post">
                     @csrf
                     <p>
-                        <laravel>名前<br>
-                        <input type="text" name="name" value="{{old('name')}}"></laravel>
+                        <label>名前<br>
+                        <input type="text" name="name" value="{{old('name')}}"></label>
                     </p>
                     <p>
-                        <laravel>生年月日<br>
-                        <input type="text" name="birthday" value="{{old('birthday')}}"></laravel>
+                        <label>生年月日<br>
+                        <input type="date" name="birthday" value="{{old('birthday')}}"></label>
                     </p>
                     <p>
-                        <laravel>住所<br>
-                        <input type="text" name="address" value="{{old('address')}}"></laravel>
+                        <label>住所<br>
+                        <input type="text" name="address" value="{{old('address')}}"></label>
                     </p>
                     <p>
-                        <laravel>電話番号<br>
-                        <input type="text" name="tel" value="{{old('tel')}}"></laravel>
+                        <label>電話番号<br>
+                        <input type="text" name="tel" value="{{old('tel')}}"></label>
                     </p>
                     <p>
-                        <laravel>メールアドレス<br>
-                        <input type="email" name="email" value="{{old('email')}}"></laravel>
+                        <label>メールアドレス<br>
+                        <input type="email" name="email" value="{{old('email')}}"></label>
                     </p>
                     <p>
-                        <laravel>パスワード<br>
-                        <input type="password" name="password" value=""></laravel>
+                        <label>パスワード<br>
+                        <input type="password" name="password" value=""></label>
                     </p>
                     <p>
-                        <laravel>パスワード（確認用）<br>
-                        <input type="password" name="password_confirmation" value=""></laravel>
+                        <label>パスワード（確認用）<br>
+                        <input type="password" name="password_confirmation" value=""></label>
                     </p>
                     <p>
                         <button type="submit">確認画面へ</button>
