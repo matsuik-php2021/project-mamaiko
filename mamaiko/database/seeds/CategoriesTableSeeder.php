f0748f77b2c8969151ed554377f9677b055121c3
matsuik-php2021 <2021php41-student022@la-study.com>
2021-05-21 14:07:33 +0900

    カテゴリ登録しーだー作成

--- mamaiko/database/seeds/CategoriesTableSeeder.php
+++ mamaiko/database/seeds/CategoriesTableSeeder.php
@@ -11,11 +11,11 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table(`categories`)->insert(['name' => 'シティホテル']);
-        DB::table(`categories`)->insert(['name' => 'リゾートホテル']);
-        DB::table(`categories`)->insert(['name' => 'ビジネスホテル']);
-        DB::table(`categories`)->insert(['name' => '旅館']);
-        DB::table(`categories`)->insert(['name' => '民宿']);
-        DB::table(`categories`)->insert(['name' => 'ペンション']);
+        DB::table('categories')->insert(['name' => 'シティホテル']);
+        DB::table('categories')->insert(['name' => 'リゾートホテル']);
+        DB::table('categories')->insert(['name' => 'ビジネスホテル']);
+        DB::table('categories')->insert(['name' => '旅館']);
+        DB::table('categories')->insert(['name' => '民宿']);
+        DB::table('categories')->insert(['name' => 'ペンション']);
     }
 }
