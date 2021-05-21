f79981d38c3c431f57a7ed455474fab52b3bd867
IsseiAdachi <issei715@gmail.com>
2021-05-21 10:33:38 +0900

    migration adachi

---
+++ mamaiko/database/migrations/2021_05_21_005451_create_plans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('description');
+            $table->integer('price');
+            $table->integer('room_count');
+            $table->bigInteger('hotel_id')->unsigned()->index();
+            $table->timestamps();
+
+            $table->foreign('hotel_id')->references('id')->on('hotels')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans');
+    }
+}
