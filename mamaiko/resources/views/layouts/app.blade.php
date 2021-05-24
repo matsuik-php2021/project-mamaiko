b367b84c2f9a7a8045486d7df9e1b6c893ec4a47
matsuik-php2021 <2021php41-student022@la-study.com>
2021-05-24 11:25:23 +0900

    ままいこからtoppageへ遷移

--- mamaiko/resources/views/layouts/app.blade.php
+++ mamaiko/resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
                         <span class="icon-bar">b</span>
                         <span class="icon-bar">c</span>
                     </button> -->
-                    <a class="navbar-brand" href="route('home')">ままいこ</a>
+                    <a class="navbar-brand" href="{{route('toppage')}}">ままいこ</a>
                     @if (Auth::check())
                     <a href="{{route('mypage')}}">マイページ</a>
                     <form name="logout" method="POST" action="{{route('logout')}}">
