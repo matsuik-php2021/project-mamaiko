5af7c434e9e48b231347a63b04647e2929429322
matsuik-php2021 <2021php41-student022@la-study.com>
2021-05-25 11:41:48 +0900

    管理者ホームぺージ作成

--- mamaiko/resources/views/admin/home.blade.php
+++ mamaiko/resources/views/admin/home.blade.php
@@ -1,8 +1,19 @@
 @extends('layouts.app')
 @section('content')
-<h1>管理者ログイン完了</h1>
-<form action="{{route('admin.logout')}}" method="post">
+<h1>管理者メニュー</h1>
+<p>
+    <a href="{{route('admin.')}}" class="btn btn-secondary">予約管理</a>
+</p>
+<p>
+    <a href="{{route('admin.')}}" class="btn btn-secondary">会員管理</a>
+</p>
+<p>
+    <a href="{{route('admin.')}}" class="btn btn-secondary">ホテル情報管理</a>
+</p>
+
+<p><form action="{{route('admin.logout')}}" method="post">
     @csrf
-    <button type="submit">ログアウト</button>
-</form>
+    <button type="submit" class="btn btn-outline-info" style="font-style: bold;">ログアウト</button>
+</form></p>
+
 @endsection
