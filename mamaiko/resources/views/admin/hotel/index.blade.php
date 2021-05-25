73f94ebc4e26be6261f24978738d6cbf1bbcf50f
matsuik-php2021 <2021php41-student022@la-study.com>
2021-05-25 16:14:21 +0900

    ホテル一覧のUI変更

--- mamaiko/resources/views/admin/hotel/index.blade.php
+++ mamaiko/resources/views/admin/hotel/index.blade.php
@@ -4,7 +4,7 @@
 <p>
     <a href="{{route('admin.hotel.create')}}" class="btn btn-secondary">新規登録</a>
 </p>
-<table class="table">
+<!-- <table class="table">
     <tr>
       <th>ID</th>
       <th>名前</th>
@@ -27,7 +27,33 @@
             <td><a href="{{route('admin.hotel.edit',$hotel->id)}}" class="btn btn-secondary">変更</a></td>
         </tr>
         @endforeach
-  </table>
+  </table> -->
+@foreach ($hotels as $hotel)
+    <div class="card">
+        <!-- <img class="card-img-top" src="/images/pathToYourImage.png" alt="Card image cap"> -->
+        <div class="card-body">
+            <h4 class="card-title">{{$hotel->name}}</h4>
+            <p class="card-text">
+                <table>
+                    <tr>
+                        <td>概要</td>
+                        <td>{{$hotel->description}}</td>
+                    </tr>
+                    <tr>
+                        <td>住所</td>
+                        <td>{{$hotel->address}}</td>
+                    </tr>
+                    <tr>
+                        <td>アクセス</td>
+                        <td>{{$hotel->access}}</td>
+                    </tr>
+                </table>
+            </p>
+            <a href="{{route('admin.hotel.edit',$hotel->id)}}" class="btn btn-secondary">変更</a>
+        </div>
+    </div>
+@endforeach
+
 {{$hotels->links()}}
 
 <a href="{{route('admin.home.index')}}" class="btn btn-info">戻る</a>
