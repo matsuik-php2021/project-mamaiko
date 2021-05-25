57ee2ef2b1ff0d61b02d727a0d450e8d62456322
matsuik-php2021 <2021php41-student022@la-study.com>
2021-05-25 12:01:44 +0900

    ビューファイルだけ作った

--- mamaiko/resources/views/admin/home.blade.php
+++ mamaiko/resources/views/admin/home.blade.php
@@ -2,18 +2,18 @@
 @section('content')
 <h1>管理者メニュー</h1>
 <p>
-    <a href="{{route('admin.')}}" class="btn btn-secondary">予約管理</a>
+    <a href="{{route('admin.')}}" class="btn btn-secondary">予約管理(未実装)</a>
 </p>
 <p>
     <a href="{{route('admin.')}}" class="btn btn-secondary">会員管理</a>
 </p>
 <p>
-    <a href="{{route('admin.')}}" class="btn btn-secondary">ホテル情報管理</a>
+    <a href="{{route('admin.')}}" class="btn btn-secondary">ホテル情報管理(未実装)</a>
 </p>
 
 <p><form action="{{route('admin.logout')}}" method="post">
     @csrf
     <button type="submit" class="btn btn-outline-info" style="font-style: bold;">ログアウト</button>
 </form></p>
-
+<p>TODO:管理者ヘッダー</p>
 @endsection
---
+++ mamaiko/resources/views/admin/usersList.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+<h1>管理者メニュー</h1>
+
+<a href="{{route('admin.home')}}" class="btn btn-info">戻る</a>
+@endsection
