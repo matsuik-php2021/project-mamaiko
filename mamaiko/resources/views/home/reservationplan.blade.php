051108ec5f20a3427096664c23aef6d8de7d71ff
IsseiAdachi <issei715@gmail.com>
2021-05-26 11:14:52 +0900

    change reservation

--- mamaiko/resources/views/home/reservationplan.blade.php
+++ mamaiko/resources/views/home/reservationplan.blade.php
@@ -6,12 +6,16 @@
         <th>ホテル名</th>
         <th>プラン名</th>
         <th>宿泊日程</th>
+        <th></th>
     </tr>
     @foreach ($reservations as $reservation)
     <tr>
         <td><a href = "{{ route('hotelshow', $reservation->hotel_id) }}">{{ $reservation->hotel->name }}</a></td>
         <td>{{ $reservation->plan->name }} </a></td>
         <td>{{ $reservation->checkin_date }} ~ {{ $reservation->checkout_date }}</td>
+        <td>
+        <a href = "{{ route('reservation.edit', $reservation->id) }}">予約変更</a>
+        </td>
     </tr>
     @endforeach
 </table>   
--- mamaiko/routes/web.php
+++ mamaiko/routes/web.php
@@ -38,6 +38,8 @@ Route::group(['middleware'=>['auth']],function(){
     Route::get('/favoriteindex', 'FavoriteController@index')->name('home.favoriteindex');
     Route::post('/favoritestore', 'FavoriteController@store')->name('favorites.store');
     Route::delete('/favoritedestroy', 'Favoritecontroller@destroy')->name('favorites.destroy');
+    Route::get('/plan/{id}/reservation/edit', 'ReservationController@edit')->name('reservation.edit');
+    Route::post('/plan/reservation/update', 'ReservationController@update')->name('reservation.update');
 });
 
 Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
